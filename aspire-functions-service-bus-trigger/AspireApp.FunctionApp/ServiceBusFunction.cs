70b24d1de34d7715330a852f3022279eae71d16b
Alexis Garon-Michaud <alexis_35115@hotmail.com>
2026-01-11 21:14:56 -0500

    mettons

--- aspire-functions-service-bus-trigger/AspireApp.FunctionApp/ServiceBusFunction.cs
+++ aspire-functions-service-bus-trigger/AspireApp.FunctionApp/ServiceBusFunction.cs
@@ -3,7 +3,6 @@ using Microsoft.Azure.Functions.Worker;
 using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
-using System.Collections.Generic;
 using System.Text.Json;
 
 namespace AspireApp.FunctionApp;
